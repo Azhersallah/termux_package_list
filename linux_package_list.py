99b494afe1814eea052c740a463d7efa1fa5d876
Azhersallah <70579615+Azhersallah@users.noreply.github.com>
2020-09-01 14:41:24 +0300

    Add files via upload

---
+++ linux_package_list.py
@@ -0,0 +1,22 @@
+import os
+
+os.system("apt update && apt upgrade && apt install php && apt install python && apt install python2 && apt install git && apt install golang && apt install host && apt install nano && apt install havij && apt install hydra && apt install wireshark && apt install cmatrix && apt install figlet && apt install wget -y && apt install python2-dav -y && apt install wireshark && apt install cowsay &&apt install toilet && apt install ruby && gem install lolcat && apt install curl && apt install wgetrc && apt install openssh && apt install tor && apt install uzip && apt install net-tools && apt install unrar && apt install clang && apt install w3m && apt install proot && pip2 install requests && apt update && apt upgrade")
+
+
+
+by="Azher S. Hassan"
+print("Created by " + by)
+print(" █████╗ ███████╗██╗  ██╗███████╗██████╗")
+print("██╔══██╗╚══███╔╝██║  ██║██╔════╝██╔══██╗")
+print("███████║  ███╔╝ ███████║█████╗  ██████╔╝")
+print("██╔══██║ ███╔╝  ██╔══██║██╔══╝  ██╔══██╗")
+print("██║  ██║███████╗██║  ██║███████╗██║  ██║")
+print("╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝")
+
+import webbrowser
+
+new=2;
+
+url="https://www.facebook.com/Azher.sallah"
+
+webbrowser.open(url,new=new);
