cb90a3770719c985439675c2c3180e80289bf17a
Azhersallah <70579615+Azhersallah@users.noreply.github.com>
2020-09-01 14:34:16 +0300

    Add files via upload

---
+++ termux_package_list.py
@@ -0,0 +1,22 @@
+import os
+
+os.system("apt update && apt upgrade && pkg install php && pkg install python && pkg install python2 && pkg install git && pkg install golang && pkg install host && pkg install nano && pkg install havij && pkg install hydra && pkg install wireshark && pkg install cmatrix && pkg install figlet && pkg install wget -y && pkg install python2-dav -y && pkg install wireshark && pkg install cowsay &&pkg install toilet && pkg install ruby && gem install lolcat && pkg install curl && pkg install wgetrc && pkg install openssh && pkg install tor && pkg install uzip && pkg install net-tools && pkg install unrar && pkg install clang && pkg install w3m && pkg install proot && pip2 install requests && pkg update && pkg upgrade")
+
+by="Azher S. Hassan"
+print("Created by " + by)
+
+print(" █████╗ ███████╗██╗  ██╗███████╗██████╗")
+print("██╔══██╗╚══███╔╝██║  ██║██╔════╝██╔══██╗")
+print("███████║  ███╔╝ ███████║█████╗  ██████╔╝")
+print("██╔══██║ ███╔╝  ██╔══██║██╔══╝  ██╔══██╗")
+print("██║  ██║███████╗██║  ██║███████╗██║  ██║")
+print("╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝")
+
+
+import webbrowser
+
+new=2;
+
+url="https://www.facebook.com/Azher.sallah"
+
+webbrowser.open(url,new=new);
